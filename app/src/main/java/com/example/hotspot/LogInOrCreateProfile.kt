3152c896ff091f7004754529b7708c9141eff0ae
Freja Jensen <s195473@student.dtu.dk>
2021-10-22 23:07:07 +0200

    First version of botttom navigation menu

--- app/src/main/java/com/example/hotspot/LogInOrCreateProfile.kt
+++ app/src/main/java/com/example/hotspot/LogInOrCreateProfile.kt
@@ -11,13 +11,12 @@ import androidx.navigation.Navigation
 
 class LogInOrCreateProfile : Fragment(), View.OnClickListener {
 
-    var navController: NavController? = null
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_log_in_or_create_profile, container, false)
     }
 
@@ -30,8 +29,8 @@ class LogInOrCreateProfile : Fragment(), View.OnClickListener {
 
     override fun onClick(p0: View?) {
         when(p0!!.id){
-            R.id.button_text -> navController!!.navigate(R.id.action_logInOrCreateProfile_to_logIn)
-            R.id.create_profile -> navController!!.navigate(R.id.action_logInOrCreateProfile_to_createProfile)
+            R.id.button_text -> navController.navigate(R.id.action_logInOrCreateProfile_to_logIn)
+            R.id.create_profile -> navController.navigate(R.id.action_logInOrCreateProfile_to_createProfile)
         }
     }
 
--- app/src/main/java/com/example/hotspot/MainActivity.kt
+++ app/src/main/java/com/example/hotspot/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.hotspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
