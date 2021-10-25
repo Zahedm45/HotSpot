af2de5a2d3fc51b120a0b946819b94e5be2b588e
Freja Jensen <s195473@student.dtu.dk>
2021-10-25 11:48:02 +0200

    xml drawable filer tilføjet

--- app/src/main/java/com/example/hotspot/MainActivity.kt
+++ app/src/main/java/com/example/hotspot/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.hotspot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import com.example.hotspot.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+/*        binding = ActivityMainBinding.inflate(LayoutInflater)
+        setContentView(binding.root)*/
     }
 }
