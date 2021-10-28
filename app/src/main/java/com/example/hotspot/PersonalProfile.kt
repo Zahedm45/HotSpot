496ce3e8c56086a0af13307d319f43fc7f4b3c2d
OlineMelindaAndersen <71652011+OlineMelindaAndersen@users.noreply.github.com>
2021-10-28 12:59:08 +0200

    added action to button edit_profile in fragment_personal_profile

--- app/src/main/java/com/example/hotspot/PersonalProfile.kt
+++ app/src/main/java/com/example/hotspot/PersonalProfile.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
-class PersonalProfile : Fragment() {
+class PersonalProfile : Fragment(),View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,5 +19,17 @@ class PersonalProfile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_personal_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.edit_profile).setOnClickListener(this)
+
+    }
+    override fun onClick(p0: View?) {
+        when (p0!!.id) {
+            R.id.edit_profile -> navController.navigate(R.id.action_personalProfile_to_editProfile)
+        }
     }
 }
