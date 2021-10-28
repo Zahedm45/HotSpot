238403e1774eaa6b08c04d6286a078ad225b885f
Lucas Skals <skalslucas@gmail.com>
2021-10-28 05:19:34 +0200

    Finished all views for chat.

--- app/src/main/java/com/example/hotspot/MessageOverview.kt
+++ app/src/main/java/com/example/hotspot/MessageOverview.kt
@@ -1,11 +1,13 @@
 package com.example.hotspot
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.cardview.widget.CardView
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
@@ -25,12 +27,13 @@ class MessageOverview : Fragment(), View.OnClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = Navigation.findNavController(view)
-        view.findViewById<Button>(R.id.log_in).setOnClickListener(this)
+      view.findViewById<CardView>(R.id.chatOnClick).setOnClickListener(this)
+
     }
 
     override fun onClick(chatsView: View?){
         when(chatsView!!.id){
-
+            R.id.chatOnClick -> navController.navigate(R.id.action_messageOverview_to_chat)
         }
     }
 }
