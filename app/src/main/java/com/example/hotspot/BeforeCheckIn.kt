856b3f6475667a1d19f6b6443931d4a487f7280a
Zahed <zahedm45@gmail.com>
2021-10-29 03:26:25 +0200

    navigated from homeMap to BeforeCheckIn

--- app/src/main/java/com/example/hotspot/BeforeCheckIn.kt
+++ app/src/main/java/com/example/hotspot/BeforeCheckIn.kt
@@ -12,7 +12,6 @@ import androidx.navigation.Navigation
 import com.example.hotspot.databinding.BeforeCheckInBinding
 
 
-
 class BeforeCheckIn : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,44 +29,3 @@ class BeforeCheckIn : Fragment() {
     }
 
 }
-
-
-
-
-
-
-
-
-//class  BeforeCheckIn : Fragment(), View.OnClickListener {
-//
-//    lateinit var navController: NavController
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return inflater.inflate(R.layout.before_check_in, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        navController = Navigation.findNavController(view)
-//        view.findViewById<Button>(R.id.checkInButton1).setOnClickListener(this)
-//    }
-//
-//    override fun onClick(p0: View?) {
-//        when(p0!!.id){
-//            R.id.checkInButton1 -> navController.navigate(R.id.action_beforeCheckIn_to_afterCheckIn)
-//        }
-//    }
-//
-//}
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/example/hotspot/HomeMap.kt
+++ app/src/main/java/com/example/hotspot/HomeMap.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.navigation.Navigation
 
 class HomeMap : Fragment() {
 
@@ -13,8 +16,13 @@ class HomeMap : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_map, container, false)
+        val view = inflater.inflate(R.layout.fragment_home_map, container, false)
 
+        view.findViewById<ImageButton>(R.id.mapImage).setOnClickListener {
+
+            Navigation.findNavController(view).navigate(R.id.beforeCheckIn)
+        }
+        return view
 
     }
 }
