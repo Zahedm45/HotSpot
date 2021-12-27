bb24fc5516a0b72a6c3fca5d290d8c15bce43912
Zahed <zahedm45@gmail.com>
2021-12-27 21:26:52 +0100

    changed login page in xml

--- app/src/main/java/com/example/hotspot/view/MainActivity.kt
+++ app/src/main/java/com/example/hotspot/view/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
