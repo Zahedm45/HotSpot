4b87e46aa7c86c952a7144a7accd5669223304db
Zahed <zahedm45@gmail.com>
2021-12-27 23:51:29 +0100

    that's all for today

--- app/src/main/java/com/example/hotspot/view/CreateProfileActivity.kt
+++ app/src/main/java/com/example/hotspot/view/CreateProfileActivity.kt
@@ -1,9 +1,12 @@
 package com.example.hotspot.view
 
+import android.app.Activity
 import android.content.ContentValues
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import com.example.hotspot.databinding.ActivityCreateProfileBinding
 import com.example.hotspot.viewModel.CreateProfileViewModel
@@ -13,6 +16,7 @@ import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.lang.Thread.sleep
 
 class CreateProfileActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -38,8 +42,28 @@ class CreateProfileActivity : AppCompatActivity() {
             createProfileVM.createNewProfile(auth)
 
         }
+
+        binding.activityCreateProfileImage.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
     }
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+
+            val uri = data.data
+            val bitMap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            val bitMDrawable = BitmapDrawable(bitMap)
+            binding.activityCreateProfileImage.setBackgroundDrawable(bitMDrawable)
+
+        }
+    }
 
     override fun onStart() {
         super.onStart()
