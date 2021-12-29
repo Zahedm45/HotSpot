43112ce37d302803d533c9d59d757a70854d2872
Zahed <zahedm45@gmail.com>
2021-12-29 11:03:05 +0100

    login info will be saved..

--- app/src/main/java/com/example/hotspot/view/LoginActivity.kt
+++ app/src/main/java/com/example/hotspot/view/LoginActivity.kt
@@ -1,8 +1,12 @@
 package com.example.hotspot.view
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Checkable
 import com.example.hotspot.databinding.ActivityLoginBinding
 import com.example.hotspot.viewModel.DataHolder
 import com.example.hotspot.viewModel.LoginActivityVM
@@ -35,18 +39,72 @@ class LoginActivity : AppCompatActivity() {
 
         loginActivityMV = LoginActivityVM(this, binding, repository)
 
+        loadLoginInfo()
+
         binding.activityLoginCreateProfileBtn.setOnClickListener {
             val intent = Intent(this, CreateProfileActivity::class.java)
             startActivity(intent)
         }
 
+
         binding.activityLoginLoginBtn.setOnClickListener {
+
             loginActivityMV.login(auth)
+
+            if (binding.activityLoginRememberMe.isChecked){
+                saveLoginInfo()
+
+            } else {
+
+                val sharedP = getSharedPreferences("userLogin", Context.MODE_PRIVATE)
+                sharedP.edit().clear().apply()
+            }
         }
+
+
+
 
 
     }
 
+
+    private fun saveLoginInfo() {
+
+        val sharedP = getSharedPreferences("userLogin", Context.MODE_PRIVATE)
+        val editor = sharedP.edit()
+        val email = binding.activityLoginEmail.text.toString()
+        val password = binding.activityLoginPassword.text.toString()
+        val isSaveChecked = binding.activityLoginRememberMe.isChecked
+
+        editor.apply {
+            putString("STRING_EMAIL", email)
+            putString("STRING_PASSWORD", password)
+            putBoolean("BOOLEAN_IS_SAVE_CHECKED", isSaveChecked)
+
+        }.apply()
+
+    }
+
+
+    private fun loadLoginInfo() {
+
+        val sharedP = getSharedPreferences("userLogin", Context.MODE_PRIVATE)
+        val isSaveChecked = sharedP.getBoolean("BOOLEAN_IS_SAVE_CHECKED", false)
+
+        if (!isSaveChecked) {
+            return
+        }
+
+
+        val email = sharedP.getString("STRING_EMAIL", null)
+        val password = sharedP.getString("STRING_PASSWORD", null)
+
+
+        binding.activityLoginEmail.setText(email)
+        binding.activityLoginPassword.setText(password)
+        binding.activityLoginRememberMe.isChecked = isSaveChecked
+
+    }
 
 
 
