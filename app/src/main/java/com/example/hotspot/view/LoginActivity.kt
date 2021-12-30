c881d55e9e091664f68d50e926cd25ec177664b6
Zahed <zahedm45@gmail.com>
2021-12-30 22:01:11 +0100

    ....

--- app/src/main/java/com/example/hotspot/view/LoginActivity.kt
+++ app/src/main/java/com/example/hotspot/view/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.hotspot.view
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
