fb3799d12e6f4885e61946f66ebe0f3179120826
Lucas Skals <skalslucas@gmail.com>
2021-12-31 16:22:24 +0100

    .xml for PhoneAuth is now finished.

--- app/src/main/java/com/example/hotspot/view/PhoneAuthActivity.kt
+++ app/src/main/java/com/example/hotspot/view/PhoneAuthActivity.kt
@@ -1,12 +1,33 @@
 package com.example.hotspot.view
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import com.example.hotspot.R
+import com.example.hotspot.databinding.ActivityPhoneAuthBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthProvider
 
 class PhoneAuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPhoneAuthBinding
+
+    private var forceResendtingToken: PhoneAuthProvider.ForceResendingToken? = null
+    private var mCallBacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks? = null
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private val TAG = "MAIN_TAG"
+
+    private lateinit var progressDialog: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_phone_auth)
+        binding = ActivityPhoneAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.enterVerificationLinearLayout.visibility = View.GONE
+
     }
 }
