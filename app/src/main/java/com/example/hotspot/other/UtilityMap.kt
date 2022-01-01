694bc4c04c3026e02a8cbb1cc307af5d21f1c869
Zahed <zahedm45@gmail.com>
2022-01-01 15:35:41 +0100

    added permission request for current location

---
+++ app/src/main/java/com/example/hotspot/other/UtilityMap.kt
@@ -0,0 +1,21 @@
+package com.example.hotspot.other
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+
+object UtilityMap {
+
+// https://www.youtube.com/watch?v=6CTIvIAHjrU
+
+
+    fun hasLocationPermission(context: Context) =
+
+        EasyPermissions.hasPermissions(
+            context,
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+}
--- app/src/main/java/com/example/hotspot/view/MapsFragment.kt
+++ app/src/main/java/com/example/hotspot/view/MapsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.hotspot.view
 
+import android.Manifest
+import android.os.Build
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
@@ -7,15 +9,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.hotspot.R
+import com.example.hotspot.other.Constants.PERMISSION_REQUEST_CODE
+import com.example.hotspot.other.UtilityMap
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(), EasyPermissions.PermissionCallbacks {
+// https://www.youtube.com/watch?v=6CTIvIAHjrU
+
+
+/*
 
     private val callback = OnMapReadyCallback { googleMap ->
 
@@ -23,18 +27,85 @@ class MapsFragment : Fragment() {
         googleMap.addMarker(MarkerOptions().position(dtu).title("Marker in DTU"))
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dtu, 11f))
     }
+*/
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps4, container, false)
+        val view = inflater.inflate(R.layout.fragment_maps4, container, false)
+
+
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+//        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+//        mapFragment?.getMapAsync(callback)
+        requestLocPermission()
+
+
     }
+
+    private fun requestLocPermission() {
+        if(UtilityMap.hasLocationPermission(requireContext())) {
+            return
+        }
+
+        EasyPermissions.requestPermissions(
+            this,
+            "You need to give location permission to run this app.",
+            PERMISSION_REQUEST_CODE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+
+    }
+
+
+
+
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestLocPermission()
+        }
+
+    }
+
+
+
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
