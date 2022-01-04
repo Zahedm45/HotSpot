c9de17420a38a6a61b153a8027b44e05744e59c1
Lucas Skals <skalslucas@gmail.com>
2022-01-04 01:43:44 +0100

    Loading page seems to be working...

--- app/src/main/java/com/example/hotspot/view/ActivityLoadingPage.kt
+++ app/src/main/java/com/example/hotspot/view/ActivityLoadingPage.kt
@@ -12,6 +12,8 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class ActivityLoadingPage : AppCompatActivity() {
@@ -29,18 +31,37 @@ class ActivityLoadingPage : AppCompatActivity() {
 
     private fun isUserLoggedIn() {
         val user = Firebase.auth.currentUser
-        var isCreated = true
+
         if (user != null) {
-            val abc = CoroutineScope(Dispatchers.IO).launch {
-                isCreated = repository.isUserProfileCreated()
+            CoroutineScope(IO).launch {
+                val isCreated = repository.isUserProfileCreated()
                 Log.d("LUCASLOFT", isCreated.toString())
                 if(isCreated) {
-                    val intent = Intent(this@ActivityLoadingPage, AfterLoginActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
-                    finish()
+                    toMapsActivity() // If profile has been created, we go to our Maps landing page.
+                }
+                else{
+                    navigateToLogin() //If no profile has been created, but user has been authed.
                 }
             }.isCompleted
         }
+        else{
+            CoroutineScope(IO).launch{
+            delay(1500)
+            navigateToLogin()} // in case of no user remember by our system, we just send them to login
+        }
+    }
+
+    private fun navigateToLogin(){
+        val intent = Intent(this@ActivityLoadingPage, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
+    }
+
+    private fun toMapsActivity(){
+        val intent = Intent(this@ActivityLoadingPage, AfterLoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
     }
 }
