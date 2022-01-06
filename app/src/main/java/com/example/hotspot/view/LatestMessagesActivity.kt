6eaea8013ed50b747a1a7127571237f9b9ff4dea
S <stevenxsx@gmail.com>
2022-01-06 19:59:24 +0100

    First section completed. Unable to compile currently.

---
+++ app/src/main/java/com/example/hotspot/view/LatestMessagesActivity.kt
@@ -0,0 +1,35 @@
+package com.example.hotspot.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import com.example.hotspot.R
+import com.google.firebase.auth.FirebaseAuth
+
+class LatestMessagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_messages)
+
+        verifyUserIsLoggedIn()
+
+
+    }
+
+    private fun verifyUserIsLoggedIn() {
+        //Perform check to see if user is logged in (if necessary)
+        val uid = FirebaseAuth.getInstance().uid
+        if (uid == null) {
+            val intent = Intent(this, RegisterActivity::class)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(menuRes, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    //Refer to 9:00 of video 4 to create menuRes
+}
