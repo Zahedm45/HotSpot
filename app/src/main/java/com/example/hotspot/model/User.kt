ce707e5f6854c7c069e10a6fcf3eadd1bb7125e8
Zahed <zahedm45@gmail.com>
2022-01-08 23:02:20 +0100

    ...

--- app/src/main/java/com/example/hotspot/model/User.kt
+++ app/src/main/java/com/example/hotspot/model/User.kt
@@ -1,12 +1,13 @@
 package com.example.hotspot.model
 
+import android.graphics.Bitmap
 import android.os.Parcelable
 
 data class User(
-    var name: String,
-    var age: Int,
-    var emailAddress: String,
+    var name: String? = null,
+    var age: Int? = null,
+    var emailAddress: String? = null,
     var bio: String? = null,
-    val gender: String,
-    var bitmapImg: String = null
+    val gender: String? = null,
+    var bitmapImg: Bitmap? = null
 ){}
--- app/src/main/java/com/example/hotspot/view/NewMessageActivity.kt
+++ app/src/main/java/com/example/hotspot/view/NewMessageActivity.kt
@@ -2,28 +2,21 @@ package com.example.hotspot.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
-import android.view.View
-import androidx.fragment.app.findFragment
-import androidx.lifecycle.ViewModel
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+
 import com.example.hotspot.R
-import com.example.hotspot.databinding.ActivityLatestMessagesBinding
 import com.example.hotspot.databinding.ActivityNewMessageBinding
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.firestore.auth.User
-import com.squareup.picasso.Picasso
+import com.example.hotspot.model.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 
+
 class NewMessageActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
+ //   private lateinit var recyclerView: RecyclerView
     private lateinit var binding: ActivityNewMessageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,29 +29,56 @@ class NewMessageActivity : AppCompatActivity() {
         fetchUsers()
     }
 
+
+
+
     private fun fetchUsers(){
-        val userRef = FirebaseDatabase.getInstance().getReference("/users")
-        userRef.addListenerForSingleValueEvent(object: ValueEventListener{
-            override fun onDataChange(p0: DataSnapshot){
+
+        val db = Firebase.firestore
+
+
+        val userRef = db.collection("users")
+
+
+        userRef.get()
+            .addOnSuccessListener {
+                val users = it.toObjects<User>()
                 val adapter = GroupAdapter<ViewHolder>()
-                p0.children.forEach {
-                    val user = it.getValue(User::class.java)
-                    if (user!=null) {
-                        adapter.add(UserItem(user))
-                    }
+                users.forEach { user ->
+                    adapter.add(UserItem(user))
                 }
-            }
-            override fun onCancelled(p0: DatabaseError){
+                binding.recyclerviewNewMessage.adapter = adapter
 
             }
-        })
+
+//        userRef.addListenerForSingleValueEvent(object: ValueEventListener{
+//            override fun onDataChange(p0: DataSnapshot){
+//                val adapter = GroupAdapter<ViewHolder>()
+//                p0.children.forEach {
+//                    val user = it.getValue(User::class.java)
+//                    if (user!=null) {
+//                        Log.i(TAG, "message. ${user.age}")
+//
+//                        adapter.add(UserItem(user))
+//                    }
+//                }
+//
+//                binding.recyclerviewNewMessage.adapter = adapter
+//
+//
+//            }
+//            override fun onCancelled(p0: DatabaseError){
+//
+//            }
+//        })
     }
 }
 
 class UserItem(val user: User): Item<ViewHolder>() {
+
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemView.row_message_name
-        Picasso.get().load(user.bitmapImg)
+ //       viewHolder.itemView
+//        Picasso.get().load(user.bitmapImg)
     }
 
     override fun getLayout(): Int {
