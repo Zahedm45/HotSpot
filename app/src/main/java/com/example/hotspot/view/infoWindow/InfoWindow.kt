5c0ff1c76d84e7e44c4392c525aa35b8eda06724
Zahed <zahedm45@gmail.com>
2022-01-09 20:46:59 +0100

    Cleaned up

--- app/src/main/java/com/example/hotspot/view/infoWindow/InfoWindow.kt
+++ app/src/main/java/com/example/hotspot/view/infoWindow/InfoWindow.kt
@@ -16,9 +16,9 @@ class InfoWindow(val context: Context) : GoogleMap.InfoWindowAdapter {
 
 
 
-    var mWindow = (context as Activity).layoutInflater.inflate(R.layout.info_window, null)
+    var window = (context as Activity).layoutInflater.inflate(R.layout.info_window, null)
 
-    private fun rendowWindowText(marker: Marker, view: View){
+    private fun setWindowText(marker: Marker, view: View){
 
         val tvTitle = view.findViewById<TextView>(R.id.info_window_title)
         val tvSnippet = view.findViewById<TextView>(R.id.info_window_snippet)
@@ -29,61 +29,13 @@ class InfoWindow(val context: Context) : GoogleMap.InfoWindowAdapter {
     }
 
     override fun getInfoContents(marker: Marker): View {
-        rendowWindowText(marker, mWindow)
-        return mWindow
+        setWindowText(marker, window)
+        return window
     }
 
     override fun getInfoWindow(marker: Marker): View? {
-        rendowWindowText(marker, mWindow)
-        return mWindow
+        setWindowText(marker, window)
+        return window
     }
 }
 
-
-
-
-
-
-
-
-
-/*
-class InfoWindow(val context: Context) : GoogleMap.InfoWindowAdapter {
-
-
-
-
-    val infoWindow = (context as Activity)
-        .layoutInflater.inflate(R.layout.info_window, null)
-
-
-
-    private fun setWindowText(marker: Marker, view: View) {
-
-//        val title = view.findViewById<TextView>(R.id.info_window_title)
-//        val desc = view.findViewById<TextView>(R.id.info_window_snippet)
-//
-//        title.text = marker.title
-//        desc.text = marker.snippet
-
-
-        val tvTitle = view.findViewById<TextView>(R.id.info_window_title)
-        val tvSnippet = view.findViewById<TextView>(R.id.info_window_snippet)
-
-        tvTitle.text = marker.title
-        tvSnippet.text = marker.snippet
-
-
-    }
-
-
-    override fun getInfoContents(p0: Marker): View? {
-        setWindowText(p0, infoWindow)
-        return infoWindow
-    }
-
-    override fun getInfoWindow(p0: Marker): View? {
-        setWindowText(p0, infoWindow)
-        return infoWindow
-    }
-}*/
