d7c6b24f5a1b8a09e7875456daa5a6f8317614a3
Lucas Skals <skalslucas@gmail.com>
2022-01-10 06:20:38 +0100

    Updated views in fragment_edit_profile.xml

--- app/src/main/java/com/example/hotspot/view/EditProfile.kt
+++ app/src/main/java/com/example/hotspot/view/EditProfile.kt
@@ -3,8 +3,12 @@ package com.example.hotspot.view
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -29,12 +33,8 @@ class EditProfile : Fragment(R.layout.fragment_edit_profile) {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        binding = FragmentEditProfileBinding.inflate(inflater,container, false)
 
-        (activity as AppCompatActivity?)!!.supportActionBar!!.apply {
-            title = "Edit Profile"
-//            setDisplayHomeAsUpEnabled(true)
-//            setDisplayShowHomeEnabled(true)
-        }
 
 
         return view
@@ -58,10 +58,11 @@ class EditProfile : Fragment(R.layout.fragment_edit_profile) {
             UtilView.signOut(this.requireActivity())
         }
 
-        binding.editProfileSaveChangeBtn.setOnClickListener {
+        binding.tvDone.setOnClickListener {
            // Log.i(TAG, "Clicked...")
             editProfileVM.updateUserProfile(bitmap)
         }
+        setGradientColor()
 
 
     }
@@ -78,6 +79,18 @@ class EditProfile : Fragment(R.layout.fragment_edit_profile) {
         }
     }
 
+    private fun setGradientColor(){
+        val paint = binding.tvDone.paint
+        val width = paint.measureText(binding.tvDone.text.toString())
+        val textShader = LinearGradient(0f, 0f, width, binding.tvDone.textSize,
+            this.resources.getColor(R.color.orange_hotspot),
+            this.resources.getColor(R.color.purple_hotspot),
+            Shader.TileMode.CLAMP
+        )
 
+        binding.tvDone.paint.setShader(textShader)
+        binding.tvDone.setTextColor(Color.RED)
+        Log.d("is this being called?", "is it?")
+    }
 
 }
