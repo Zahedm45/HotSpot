2edc14c512a5792e24dfea9185a9bd2596eecfe7
Zahed <zahedm45@gmail.com>
2022-01-12 13:42:30 +0100

    Part of the design of AfterCheckIn.kt is done

--- app/src/main/java/com/example/hotspot/view/AfterCheckIn.kt
+++ app/src/main/java/com/example/hotspot/view/AfterCheckIn.kt
@@ -1,13 +1,19 @@
 package com.example.hotspot.view
 
+import android.opengl.Visibility
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.hotspot.R
+import com.example.hotspot.databinding.BeforeCheckInBinding
+import com.example.hotspot.databinding.FragmentAfterCheckInBinding
 
 class AfterCheckIn : Fragment() {
+
+    private lateinit var binding: FragmentAfterCheckInBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +21,32 @@ class AfterCheckIn : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_after_check_in, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAfterCheckInBinding.bind(view)
+
+
+        binding.afterCheckInFavoriteBtnWhite.setOnClickListener {
+            binding.afterCheckInFavoriteBtnWhite.visibility = View.GONE
+            binding.afterCheckInFavoriteBtnThemeColor.visibility = View.VISIBLE
+
+        }
+
+
+        binding.afterCheckInFavoriteBtnThemeColor.setOnClickListener {
+            binding.afterCheckInFavoriteBtnThemeColor.visibility = View.GONE
+            binding.afterCheckInFavoriteBtnWhite.visibility = View.VISIBLE
+
+        }
+
+
+
+
+
+
     }
 
 }
