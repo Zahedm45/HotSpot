a7538151cf34750274241f21220df2bc6330ce41
OlineMelindaAndersen <71652011+OlineMelindaAndersen@users.noreply.github.com>
2022-01-13 22:39:15 +0100

    retrive data from reference is currently working

--- app/src/main/java/com/example/hotspot/view/Favorites.kt
+++ app/src/main/java/com/example/hotspot/view/Favorites.kt
@@ -8,9 +8,12 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.hotspot.R
 import com.example.hotspot.databinding.FragmentFavoritesBinding
+import com.example.hotspot.model.Address
 import com.example.hotspot.model.HotSpot
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.firestore.ktx.toObjects
 import com.google.firebase.ktx.Firebase
 import com.xwray.groupie.GroupAdapter
@@ -53,22 +56,35 @@ class Favorites : Fragment() {
 
     }
 
-
     private fun fetchFavoriteHotspots(){
         val db = Firebase.firestore
         val fbUser = Firebase.auth.uid.toString()
-        val favoriteHotspotRef = db
+        val favoriteHotspotsRef = db
             .collection("users").document(fbUser)
             .collection("favoriteHotspots")
 
-        favoriteHotspotRef.get()
-            .addOnSuccessListener {
-                val hotspots = it.toObjects<HotSpot>()
+        favoriteHotspotsRef.get()
+            .addOnSuccessListener { qr ->
+                val hotspots = qr.documents
                 val adapter = GroupAdapter<GroupieViewHolder>()
                 hotspots.forEach{hotSpot ->
-                adapter.add(HotSpotItem(hotSpot))
+                    resolveHotspotRef((hotSpot.get("hotspot") as DocumentReference).id, adapter)
                 }
                 binding.RVfavorites.adapter = adapter
+            }
+    }
+    private fun resolveHotspotRef(ref : String, adapter: GroupAdapter<GroupieViewHolder>){
+        val db = Firebase.firestore
+        val favoriteHotspotsRef = db
+            .collection("hotSpots").document(ref)
+        favoriteHotspotsRef.get()
+            .addOnSuccessListener { task ->
+                if (task.exists()) {
+                    val hotspot = task.toObject<HotSpot>()
+                    if (hotspot is HotSpot) {
+                        adapter.add(HotSpotItem(hotspot))
+                    }
+                }
             }
     }
 
@@ -82,8 +98,8 @@ class Favorites : Fragment() {
             viewHolder.itemView.deleteButton.setOnClickListener{
                 // TODO: should delete item from list of favorite hotspots + update the database
             }
-            viewHolder.itemView.hotspot_name.text = hotspot.hotSpotName
-            viewHolder.itemView.hotspot_rating.text = hotspot.overallRating.toString()
+            viewHolder.itemView.hotspot_name.text = hotspot.name
+            viewHolder.itemView.hotspot_rating.text = hotspot.rating.toString()
             viewHolder.itemView.hotspot_location.text = hotspot.address?.town
         }
 
