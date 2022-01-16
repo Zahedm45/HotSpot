1fd19fc6aa4e9672e6e16d0ad85ee918427e83e0
Zahed <zahedm45@gmail.com>
2022-01-16 22:24:45 +0100

    Cleaned up

--- app/src/main/java/com/example/hotspot/viewModel/AfterCheckInVM.kt
+++ app/src/main/java/com/example/hotspot/viewModel/AfterCheckInVM.kt
@@ -1,27 +1,17 @@
 package com.example.hotspot.viewModel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.example.hotspot.model.CheckedInDB
 import com.example.hotspot.model.HotSpot
 import com.example.hotspot.model.User
-import com.example.hotspot.other.network.TAG
 import com.example.hotspot.repository.Repository
 import com.google.firebase.firestore.ListenerRegistration
 
 
-/*typealias users = MutableList<User>
-typealias ids = MutableList<String>
-
-typealias usersAndIds = MutableList<String>*/
 
 class AfterCheckInVM {
 
     companion object {
         var checkedInListenerRig: ListenerRegistration? = null
-      //  private var isInterested : MutableLiveData<Boolean> = MutableLiveData(true)
-
         fun setListenerToCheckedInListDB(hotSpot: HotSpot) {
             if (hotSpot.id != null) {
 
@@ -33,10 +23,7 @@ class AfterCheckInVM {
 
 
         private fun onSuccessSnapShotIds(newCheckedIn: ArrayList<CheckedInDB>) {
-
             val oldCheckedIn = UsersAndIds.checkedInMap
-
-          //  Log.i(TAG, "AfterCheckedInMv")
             for (curr in newCheckedIn) {
 
                 curr.id?.let {
@@ -71,12 +58,7 @@ class AfterCheckInVM {
             if (!toRemove.isNullOrEmpty()){
                 UsersAndIds.removeUser(toRemove)
             }
-
-
-
         }
-
-
 
 
         private fun onnSuccessGetUser(user: User, checkedInDB: CheckedInDB) {
@@ -84,57 +66,13 @@ class AfterCheckInVM {
         }
 
 
-
-
         fun setIsInterested(isInterested: Boolean, hotSpotId: String) {
             val userId = DataHolder.currentUser?.uid
-
             if (userId != null) {
                 Repository.updateIsInterestedDB(hotSpotId, userId, isInterested)
             }
-
-
-
-
-
- /*           if (this.isInterested.value != isInterested) {
-                this.isInterested.postValue(isInterested)
-                updateIsInterestedDB(isInterested, hotSpot)
-
-            }*/
         }
 
-
-
-
-        private fun updateIsInterestedDB(isInterested: Boolean, hotSpot: HotSpot) {
-
-/*            val userId = Firebase.auth.uid
-            hotSpot.checkedIn?.let {
-                for (checkedIn in it) {
-                    if (checkedIn.id == userId){
-                        checkedIn.isInterested = isInterested
-                        break
-                    }
-                }
-            }
-
-            hotSpot.id?.let { hotSpot.checkedIn?.let { it1 ->
-                Repository.updateIsInterestedDB(it,
-                    it1
-                )
-            } }*/
-
-        }
-
-
-
-
-
-
-
-
-       // fun getIsInterested() = isInterested as LiveData<Boolean>
     }
 
 
