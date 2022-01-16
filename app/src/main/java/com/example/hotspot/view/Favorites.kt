7e41099a222b7f8bac075aaed9f481e042e36c6b
OlineMelindaAndersen <71652011+OlineMelindaAndersen@users.noreply.github.com>
2022-01-16 04:05:32 +0100

    small touchups

--- app/src/main/java/com/example/hotspot/view/Favorites.kt
+++ app/src/main/java/com/example/hotspot/view/Favorites.kt
@@ -1,9 +1,7 @@
 package com.example.hotspot.view
 
-import android.content.ContentValues.TAG
-import android.content.Intent
+
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
