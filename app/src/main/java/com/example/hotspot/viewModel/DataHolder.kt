38709d8bbfc5e4385aec4f0ce2b50c8ed835cd4d
Zahed <zahedm45@gmail.com>
2022-01-19 18:28:16 +0100

    ...

--- app/src/main/java/com/example/hotspot/viewModel/DataHolder.kt
+++ app/src/main/java/com/example/hotspot/viewModel/DataHolder.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import com.example.hotspot.model.CheckedInDB
 import com.example.hotspot.model.User
 import com.example.hotspot.repository.Repository
+import com.example.hotspot.repository.SubRepository
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -11,25 +12,39 @@ import com.google.firebase.ktx.Firebase
 class DataHolder {
 
     companion object {
-        var currentUser: User? = null
+       // var currentUser: User? = null
 
-        var currentUser2 = MutableLiveData<User>()
+        var currentUser = MutableLiveData<User>()
 
 
         fun fetchCurrentUserFromDB() {
             val userId = Firebase.auth.uid
             if (userId != null) {
 
+
+                SubRepository.getAndListenCurrentUserDB(userId) { user -> onSuccessUserListener(user) }
+
+
+/*
                 val checkedInDB = CheckedInDB(id = userId)
 
                 Repository.getCheckedInUserFromDB(userId, checkedInDB) {
-                        user, checkedIn -> addToCheckedInUsersList(user, checkedIn) }
+                        user, checkedIn -> addToCheckedInUsersList(user, checkedIn) }*/
             }
         }
 
 
         private fun addToCheckedInUsersList(user: User, checkedInDB: CheckedInDB) {
-            currentUser = user
+           // currentUser = user
+        }
+
+
+        private fun onSuccessUserListener(user: User) {
+
+            if (currentUser.value != user) {
+                currentUser.value = user
+            }
+
         }
 
 
