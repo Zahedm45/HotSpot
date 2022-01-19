47ffdcf86da90d5b0b6b0993da00d0dd264859a7
MikeHeltJensen <s205865@student.dtu.dk>
2022-01-19 03:23:33 +0100

    working on the reviews.kt atm

---
+++ app/src/main/java/com/example/hotspot/model/reviewItem.kt
@@ -0,0 +1,8 @@
+package com.example.hotspot.model
+
+data class reviewItem(
+    var reviewers_name: String? = null,
+    var Locations_name: String? = null,
+    var Review: String? = null
+) {
+}
--- app/src/main/java/com/example/hotspot/reviews.kt
+++ app/src/main/java/com/example/hotspot/reviews.kt
@@ -5,55 +5,80 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.hotspot.databinding.FragmentReviewsBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [reviews.newInstance] factory method to
- * create an instance of this fragment.
- */
 class reviews : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentReviewsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
+        //fetchReviews()
+        //Log.i(TAG, "oncreate")
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reviews, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment reviews.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            reviews().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentReviewsBinding.inflate(layoutInflater)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //fetchReviews()
+    }
+
+    private fun fetchReviews(){
+        val db = Firebase.firestore
+        val fbUser = Firebase.auth.uid.toString()
+        val HotspotReviews = db
+            .collection("users").document(fbUser)
+            .collection("Reviews")
+
+
+        HotspotReviews.get()
+            .addOnSuccessListener { qr ->
+                val reviews = qr.documents
+                val adapter = GroupAdapter<GroupieViewHolder>()
+                reviews.forEach { review ->
+                    resolveHotspotreview(hotSpot.get("hotspotId") as String, adapter)
                 }
+                binding.recyclerViewReviews.adapter = adapter
             }
     }
+}
+
+    /*
+
+
+    private fun resolveHotspotRef(ref : String, adapter: GroupAdapter<GroupieViewHolder>){
+        val db = Firebase.firestore
+        val favoriteHotspotsRef = db
+            .collection("hotSpots3").document(ref)
+        favoriteHotspotsRef.get()
+            .addOnSuccessListener { task ->
+                if (task.exists()) {
+                    val hotspot = task.toObject<HotSpot>()
+                    if (hotspot is HotSpot) {
+
+                        adapter.add(HotSpotItem(hotspot))
+                    }
+                }
+            }
+    */
+
 }
