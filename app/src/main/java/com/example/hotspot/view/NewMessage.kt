20d47ea41024a1a12b5928f875d7613cce5137c9
Freja Jensen <s195473@student.dtu.dk>
2022-01-19 22:17:59 +0100

    small fixes

--- app/src/main/java/com/example/hotspot/view/NewMessage.kt
+++ app/src/main/java/com/example/hotspot/view/NewMessage.kt
@@ -29,7 +29,7 @@ class NewMessage : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?) : View? {
         val view = inflater.inflate(R.layout.fragment_new_message,container, false)
-        binding = FragmentNewMessageBinding.inflate(inflater, container, false)
+        binding = FragmentNewMessageBinding.bind(view)
 
         fetchUsers()
 
